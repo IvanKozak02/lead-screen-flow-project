0838cffb2e73de6e3aa985629377f7d597bcc47e
ivan <ivasykkozak611@gmail.com>
2024-10-17 16:09:30 +0300

    update api

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.json());
 
 app.use('/app/v1/leads',leadRouter);
 
+app.get('/', (req, res) => {
+    return res.json({message: 'Hello'});
+})
 
 app.listen(3001, () => {
     console.log('APP IS RUNNING ON PORT 3001');
