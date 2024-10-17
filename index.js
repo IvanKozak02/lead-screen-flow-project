6205f7ee2bd03b5ad5e48edb9b868162917eb4d7
ivan <ivasykkozak611@gmail.com>
2024-10-17 16:11:52 +0300

    update api

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const leadRouter = require('./routes/lead-route');
 const cors = require('cors');
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/app/v1/leads',leadRouter);
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Hello'});
+    const leadData = JSON.parse(fs.readFileSync('C://Users//Ivan//WebstormProjects//leads-screen-flow//data//leads-data.json', 'utf-8'));
+    if (leadData !== ''){
+        res.status(200).json(leadData);
+    }
+})
+
+app.post('/', (req, res) => {
+    const leadData = JSON.parse(fs.readFileSync('C://Users//Ivan//WebstormProjects//leads-screen-flow//data//leads-data.json', 'utf-8'));
+    leadData.push(req.body);
 })
 
 app.listen(3001, () => {
